040722050526f0fc2990b1de1d39958541aadf1b
Viacheslav <viakostee@gmail.com>
2024-08-31 02:48:40 +0200

    Added adaptation for pc and phone

--- konko_front/src/comp/SofaComp/SofaComp.jsx
+++ konko_front/src/comp/SofaComp/SofaComp.jsx
@@ -5,7 +5,7 @@ export default function SofaComp({char, isSelected, onSelect}){
   return (
     <>
       <div className={`sofa_comp ${isSelected ? 'selected' : ''}`} onClick={onSelect}>
-        <img src={char.img} alt="sofa" />
+        <img className='sofa_image' src={char.img} alt="sofa" />
         <div className='sofa_text'>
             <h1 className='sofa_name'>{char.name}</h1>
             <p className='sofa_desc'>{char.desc}</p>
